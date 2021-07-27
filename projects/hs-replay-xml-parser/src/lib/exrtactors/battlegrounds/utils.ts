8d3988316a9b52d77b0f4868524a25ec92389409
sebastientromp <sebastien.tromp@gmail.com>
2021-07-27 21:25:36 +0200

    normalize BG hero card id to ignore skins

--- projects/hs-replay-xml-parser/src/lib/exrtactors/battlegrounds/utils.ts
+++ projects/hs-replay-xml-parser/src/lib/exrtactors/battlegrounds/utils.ts
@@ -1,4 +1,14 @@
 export const normalizeHeroCardId = (heroCardId: string): string => {
+	if (!heroCardId) {
+		return heroCardId;
+	}
+
+	// Generic handling of BG hero skins, hoping they will keep the same pattern
+	const bgHeroSkinMatch = heroCardId.match(/(.*)_SKIN_.*/);
+	if (bgHeroSkinMatch) {
+		return bgHeroSkinMatch.groups[1];
+	}
+
 	if (heroCardId === 'TB_BaconShop_HERO_59t') {
 		return 'TB_BaconShop_HERO_59';
 	}
